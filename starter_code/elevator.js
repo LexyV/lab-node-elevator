a0f105744f1bfb57a387f038c0b96d44ed5d3b42
Lexy Vanderford <alexysvanderford@gmail.com>
2018-02-13 09:29:36 -0500

    implemented changes

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,66 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = '';
+    this.intervalId;
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.intervalId = setInterval(() => { this.update(); }, 1000);
+  }
+
+  stop() { 
+   clearInterval(this.intervalId);
+  }
+
+  update() { 
+    this.log();
+    this._passengersEnter();
+    this._passengersLeave();
+  }
+
+  _passengersEnter() { 
+    if(this.waitingList.length !== 0) {
+        let personEntering = this.waitingList.filter(
+          passenger => passenger.originFloor === this.floor
+        )};
+  }
+
+  _passengersLeave() { 
+
+  }
+
+  floorUp() { 
+    if (this.floor < this.MAXFLOOR) {
+      this.floor += 1;
+      this.direction = "up";
+    }else{
+      console.log("you can't go higher");
+    }
+  }
+
+  floorDown() { 
+    if (this.floor > 0) {
+      this.floor -= 1;
+      this.direction = "down";
+    }else{
+      console.log("you can't go lower");
+    }
+  }
+
+  call(person) { 
+    this.waitingList.push(person);
+    this.requests.push(person.originFloor);
+  }
+
+  log() { 
+    console.log(`Direction: ${this.direction}, | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
+
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,12 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let lexy = new Person("Lexy", 3, 9);
+let john = new Person("John", 5, 1);
+let dumbo = new Person("Dumbo", 9, 3);
+let shelby = new Person("Shelby", 2, 7);
+
+let elevator = new Elevator();
+elevator.floorUp();
+elevator.floorDown();
+elevator.start();
